2e2b8f76815a2ec25961e4a5e7fa22a67f91dde3
Swapnil Srivastava <srivastavas@g.cofc.edu>
2023-04-10 15:37:50 -0400

    Banner implementation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import monkey from './monkey_cheers.gif';
+import kid_goku from './kid_goku.gif';
+import happy from './happy.gif';
+import wolf from './wolf_howling.gif';
+import spaceship from './spaceship.gif';
+import skull_guy from './skull_guy.gif';
 import './App.css';
-
+import Banner from './Banner';
 function App() {
   return (
     <div className="App">
+      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img className='Banner-images' src={spaceship} alt="Spaceship" />
+        <img className='Banner-images' src={wolf} alt="wolf" />
+        <img className='Banner-images' src={happy} alt="happy" />
+        <img className='Banner-images' src={monkey} alt="logo" />
+        <img className='Banner-images' src={kid_goku} alt="kid goku" />
+        <img className='Banner-images' src={skull_guy} alt="Heman villain" />
       </header>
+      <Banner />
     </div>
   );
 }
---
+++ src/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Banner.css';
+function Banner(){
+    return(
+        <div className='Banner-general'>
+            <h1>~ The Happy Monkey Universe ~</h1>
+        </div>
+    )
+}
+
+export default Banner
