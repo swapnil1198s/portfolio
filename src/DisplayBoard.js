244f16cc870981effe1f6f9c699b994250227079
Swapnil Srivastava <srivastavas@g.cofc.edu>
2023-04-13 18:45:45 -0400

    Updates to slide content and style

--- src/DisplayBoard.js
+++ src/DisplayBoard.js
@@ -1,25 +1,31 @@
 import React from "react";
 import 'react-slideshow-image/dist/styles.css';
 import { Slide } from 'react-slideshow-image';
+import game_image from './images/2d_game_screenshot.png'
+import poetry_bg from './images/poetry_bg.jpeg'
 import './custom_slide.css';
 
 function DisplayBoard(){
     const images = [
-        "https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
-        "https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80",
+        game_image,
+        poetry_bg,
         "https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
     ];
     return(
         <div className="board">
             <Slide autoplay={true}>
-                <div className="each-slide-effect">
-                    <div style={{ 'backgroundImage': `url(${images[0]})` }}>
-                        <span>Slide 1</span>
+                <div className="each-slide-effect game-slide">
+                    <div style={{ 'backgroundImage': `url(${images[0]})`}}>
+                        <span>
+                            <a href="https://github.com/swapnil1198s/Shoot-forest-shoot-" target="_blank">
+                            Play This 2D Shooting Game As It Is Being Developed
+                            </a>
+                        </span>
                     </div>
                 </div>
                 <div className="each-slide-effect">
                     <div style={{ 'backgroundImage': `url(${images[1]})` }}>
-                        <span>Slide 2</span>
+                        <span>Read Poems Written By Swapnil Srivastava</span>
                     </div>
                 </div>
                 <div className="each-slide-effect">
